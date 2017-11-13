02faecf8243dfda5ae859e4cd221bb0de881d71e
isaac bustos <isaac.youthboard@futureindesign.org>
2017-11-13 11:12:04 -0700

    add casino gemfile and wallet

---
+++ wallet.rb
@@ -0,0 +1,10 @@
+class wallet
+attr_accessor :amount
+def initialize (amount)
+ @amount = amount
+end
+
+a method to add and suntract from amout
+this can be used to add or remove bet ammounts
+
+end
